41ef6b27aa41abae36fd7f12b22874152ac10bec
Volodymyr <sainchukvova2104@gmail.com>
2025-08-13 20:47:20 +0300

    fix bugs vercel

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,10 @@
 import { Suspense } from "react";
-import css from "./NotFound.module.css";
+import NotFoundContent from "@/components/NotFoundContent/NotFoundContent";
 
 export default function NotFound() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
+      <NotFoundContent />
     </Suspense>
   );
 }
