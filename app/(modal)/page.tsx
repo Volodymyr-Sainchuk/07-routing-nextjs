e3b6c6fb23f0537b426590d84cdb331260dc0b91
Volodymyr <sainchukvova2104@gmail.com>
2025-08-13 23:35:56 +0300

    fix strucure bugs 5

--- app/(modal)/page.tsx
+++ app/(modal)/page.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 
-const NotePreview = dynamic(() => import("@/components/NotePreview/NotePreview"), { ssr: false });
+const NotePreview = dynamic(() => import("@/components/NotePreview/NotePreview"), {
+  ssr: false,
+});
 
 export default function ModalPage({ params }: { params: { slug: string[] } }) {
   const id = params.slug[params.slug.length - 1];
