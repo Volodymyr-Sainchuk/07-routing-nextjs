593845f4d7d278c99835501466f681a676b8b503
Volodymyr <sainchukvova2104@gmail.com>
2025-08-13 23:45:32 +0300

    fix strucure bugs 6

--- app/(modal)/notes/page.tsx
+++ app/(modal)/notes/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from "react";
 import { fetchNotes } from "@/lib/api";
 import Notes from "./Notes.client";
 
 export default async function NotesPage() {
   const initialData = await fetchNotes({ query: "", page: 1, perPage: 12 });
 
-  return <Notes initialData={initialData} />;
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <Notes initialData={initialData} />
+    </Suspense>
+  );
 }
