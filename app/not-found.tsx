aa9eb235630b8f12be0a8e5313f0c030a591defc
Volodymyr <sainchukvova2104@gmail.com>
2025-08-13 20:52:20 +0300

    fix bugs vercel 2

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
 import NotFoundContent from "@/components/NotFoundContent/NotFoundContent";
 
 export default function NotFound() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <NotFoundContent />
-    </Suspense>
-  );
+  return <NotFoundContent />;
 }
