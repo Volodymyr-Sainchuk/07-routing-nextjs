ac4b2c37b632e10b1f477bca07d28759ec0bbaa9
Volodymyr <sainchukvova2104@gmail.com>
2025-08-14 21:07:59 +0300

    fix bugs 2

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -3,19 +3,20 @@ import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query
 import NotePreviewClient from "@/components/NotePreview/NotePreview";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ noteId: string }>;
 };
 
 export default async function ModalPage({ params }: Props) {
+  const { noteId } = await params;
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
-    queryKey: ["note", params.id],
-    queryFn: () => fetchNoteById(params.id),
+    queryKey: ["note", noteId],
+    queryFn: () => fetchNoteById(noteId),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotePreviewClient id={params.id} />
+      <NotePreviewClient id={noteId} />
     </HydrationBoundary>
   );
 }
